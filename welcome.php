91e7c562bdcc48a6a4e4055cb1aa5a5392a357e2
rjpacket <jimbo922@163.com>
2017-07-14 19:25:12 +0800

    update

---
+++ welcome.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+<?php
+// 定义变量并设置为空值
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $website = test_input($_POST["website"]);
+    $comment = test_input($_POST["comment"]);
+    $gender = test_input($_POST["gender"]);
+    echo "welcome" ,$name,$email;
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+</body>
+</html>
